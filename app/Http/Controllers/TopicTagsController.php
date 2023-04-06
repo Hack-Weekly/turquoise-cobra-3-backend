e7141f05b123a5606001a467a2ac8304f470c923
Bhavil Jain <bhaviljain68@gmail.com>
2023-04-06 23:39:30 +0530

    Create Controller,Model,Requests for Topic Tags

---
+++ app/Http/Controllers/TopicTagsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTopicTagRequest;
+use App\Http\Requests\UpdateTopicTagRequest;
+use App\Models\TopicTag;
+
+class TopicTagsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTopicTagRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TopicTag $topicTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTopicTagRequest $request, TopicTag $topicTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TopicTag $topicTag)
+    {
+        //
+    }
+}
