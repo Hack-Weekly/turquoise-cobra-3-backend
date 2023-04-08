2068f9be0a62c64ba98eabcf617e8d245fff9dee
Bhavil Jain <bhaviljain68@gmail.com>
2023-04-08 12:37:14 +0530

    edited theme on profile edit page

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -25,20 +25,20 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="p-4 sm:p-8 shadow sm:rounded-lg content-card">
                     <UpdateProfileInformationForm
                         :must-verify-email="mustVerifyEmail"
                         :status="status"
-                        class="max-w-xl"
+                        class=""
                     />
                 </div>
 
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <UpdatePasswordForm class="max-w-xl" />
+                <div class="p-4 sm:p-8 content-card shadow sm:rounded-lg">
+                    <UpdatePasswordForm class="" />
                 </div>
 
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <DeleteUserForm class="max-w-xl" />
+                <div class="p-4 sm:p-8 content-card shadow sm:rounded-lg">
+                    <DeleteUserForm class="" />
                 </div>
             </div>
         </div>
